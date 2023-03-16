972a1940ed7aee1666e5418b7d629cc0df5a01ea
Дмитрий <d.xoxrin2010@yandex.ru>
2023-03-16 14:10:05 +0300

    no lombok

--- src/main/java/com/example/SpringSecurityGB/enity/Role.java
+++ src/main/java/com/example/SpringSecurityGB/enity/Role.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 import javax.persistence.*;
 
 @Entity
-@Data
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
     @Id
@@ -23,5 +22,29 @@ public class Role implements GrantedAuthority {
     @Override
     public String toString() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
     }
 }
--- src/main/java/com/example/SpringSecurityGB/enity/User.java
+++ src/main/java/com/example/SpringSecurityGB/enity/User.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Data
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
@@ -47,5 +46,58 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String email, Collection<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
     }
 }
